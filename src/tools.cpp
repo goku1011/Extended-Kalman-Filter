53e25c0f0beddb9025db005d2aa7e8daad22ffea
Gokul Arvind <gokul.blaze94@gmail.com>
2018-01-28 20:14:20 -0500

    Add files via upload

---
+++ src/tools.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    
+    if(estimations.size()==0||(estimations.size()!=ground_truth.size()){
+        cout<<"Invalid estimation ground truth data"<<endl;
+        return rmse;
+    }
+    for (int i=0;i<estimations.size();i++)
+    {
+        VectorXd c = estimations[i] - ground_truth[i];
+        c = c.array()*c.array();
+        rmse += c;
+    }
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
+}
+
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */MatrixXd Hj(3,4);
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+    
+    float c1 = px*px+py*py; 
+    float c2 = sqrt(c1);
+    float c3 = (c1*c2);
+    
+    if (fabs(c1) <= 0.0001){
+        cout << "error";
+	return Hj;
+    }
+    //compute the Jacobian matrix
+    Hj << (px/c2), (py/c2), 0, 0,
+	  -(py/c1), (px/c1), 0, 0,
+	  py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+    return Hj;
+}
